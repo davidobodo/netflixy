264e609ff9d11afe4cab3f0dc8a0e3a3ad5b1581
David Obodo <obododavid5@gmail.com>
2019-08-06 11:14:56 +0100

    inserting handleActions to handle multiple actions in reducer

---
+++ src/reducers/movies.js
@@ -0,0 +1,11 @@
+import {handleActions} from 'redux-actions'
+import { GET_MOVIES } from '../constants/actionTypes';
+//handleActions handles multiple actions as opposed to handleAction that handles only just one action
+
+import {GET_MOVIES} from '../constants/actionTypes'
+
+export default handleActions({
+    [GET_MOVIES]:(state, action) => state// the square bracket is because that is how variables are used as object names in ES6
+    },//key value pair of action types and reducers
+    {}//initial state
+)
