a174965d91e5b4b8f496550e9484bb1afb69c065
David Obodo <obododavid5@gmail.com>
2019-08-06 11:53:06 +0100

    included devtools store enhancer into app using compose

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -2,9 +2,18 @@ import {createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import rootReducer from '../reducers'
 import api from '../middleware/api'
+import DevTools from '../containers/DevTools'
 
 const configureStore = (initialState) => {
-    const store = createStore(rootReducer, initialState, applyMiddleware(logger, api))
+    const store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(logger, api),
+            DevTools.instrument()//Instrument function invoked as a store enhancer
+            ) 
+        );
+
     if(module.hot){
         //enable webpack hot module replacement for reducer
         module.hot.accept('../reducers', () => {
