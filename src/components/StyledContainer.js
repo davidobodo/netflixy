6be660731743f89a06b7335d2ed05be0936f7746
David Obodo <obododavid5@gmail.com>
2019-08-06 22:05:15 +0100

    added some global styles to the app through a newly created file

--- src/components/StyledContainer.js
+++ src/components/StyledContainer.js
@@ -3,6 +3,18 @@ import styled from 'styled-components'
 const StyledContainer = styled.div`
     background: #333852;
     min-height:1000vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+
+    &:before{
+        content:"NETFLIXY";
+        position:fixed;
+        top:18vh;
+        left:10vw;
+        font-size:9vw;
+        color:rgba(255,255,255,0.03);
+    }
 `;
 
 export default StyledContainer
