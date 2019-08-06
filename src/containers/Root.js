fd7f345f1767680ab68b5cc92cbfbfa999f463b8
David Obodo <obododavid5@gmail.com>
2019-08-06 22:30:54 +0100

    added another styled component

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -3,13 +3,17 @@ import {Provider} from 'react-redux'
 import store from '../store/store'
 import DevTools from './DevTools'
 import StyledContainer from '../components/StyledContainer'
+import { ThemeProvider } from 'styled-components'
+import theme from '../constants/theme'
 
 const Root = () => {
     return(
         <Provider store={store}>
-            <StyledContainer>
-                <DevTools/>
-            </StyledContainer>
+            <ThemeProvider theme={theme}>
+                <StyledContainer>
+                    <DevTools/>
+                </StyledContainer>
+            </ThemeProvider>
         </Provider>
     )
 }
