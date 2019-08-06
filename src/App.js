4dbcf876664099c92bb3fc45e98212f6129ef97b
David Obodo <obododavid5@gmail.com>
2019-08-06 11:23:06 +0100

    added two files into the store folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+ dhsbsbkdbdsjkdskbjdskb
+      </div>
+    );
+
+  }
+ 
 }
 
 export default App;
