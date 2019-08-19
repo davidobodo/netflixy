859079045d922cbf4450418d41c46b04bbaa9f58
David Obodo <obododavid5@gmail.com>
2019-08-19 19:01:21 +0100

    styled the link in my movie details

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import StyledHeader from './StyledHeader'
+import FontAwesomeIcon from './FontAwesomeIcons'
+import StyledHeaderLink from './StyledHeaderLink'
 
-const MovieDetails =()=> {
-    return <div>MovieDetails</div>
+const MovieDetails = () => {
+    return (
+        <StyledHeader>
+            <StyledHeaderLink href="/">
+                <FontAwesomeIcon icon="chevron-left" text="Go back" />
+            </StyledHeaderLink>
+        </StyledHeader>
+    )
 }
 
 export default MovieDetails
