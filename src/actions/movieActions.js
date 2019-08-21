b7e1d92780f242a6dadc8da7b3753725d4066eba
David Obodo <obododavid5@gmail.com>
2019-08-21 16:53:41 +0100

    successfully outputting data in my ui

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,6 +1,8 @@
 import {createAction} from 'redux-actions'
 import {apiPayloadCreator} from '../util/appUtils'
 import {API} from '../constants/actionTypes'
+import {normalize,schema} from 'normalizr'
+import { SET_MOVIES} from '../constants/actionTypes'
 
 const getMoviesAC =createAction(API, apiPayloadCreator);
 
@@ -8,6 +10,11 @@ export const getMovies =()=>
     getMoviesAC({url:"/vcvx0", onSuccess: setMovies});
 
 function setMovies(movies){
-    console.log(movies);
-    return{type:""};
+    const movieSchema = new schema.Entity("movies");
+    const movieListSchema = new schema.Array(movieSchema);
+    const normalizedData = normalize(movies, movieListSchema);
+    return{
+        type: SET_MOVIES,
+        payload:normalizedData.entities.movies
+    };
 }
--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -1,9 +1,11 @@
 import {handleActions} from 'redux-actions'
 import { GET_MOVIES } from '../constants/actionTypes';
+import { SET_MOVIES } from '../constants/actionTypes'
 //handleActions handles multiple actions as opposed to handleAction that handles only just one action
 
 export default handleActions({
-    [GET_MOVIES]:(state, action) => state// the square bracket is because that is how variables are used as object names in ES6
+    [GET_MOVIES]:(state, action) => state,// the square bracket is because that is how variables are used as object names in ES6
+    [SET_MOVIES]:(state,action)=>action.payload
     },//key value pair of action types and reducers
     {}//initial state
 )
